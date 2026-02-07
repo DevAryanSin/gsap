b3a758d1f452d62c0158186260da87dd1974ca3e
DevAryanSin <pineapplepenguin30@gmail.com>
2026-02-07 13:38:02 +0530

    Font Family and Colors Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main className="min-h-screen p-10 flex flex-col gap-6">
+            <h1 className="text-5xl text-orange-500 font-bold font-zentry">Zentry Font</h1>
+            <h1 className="text-5xl text-blue-500 font-bold font-general">General Font</h1>
+            <h1 className="text-5xl text-green-500 font-bold font-robert-medium">Robert Medium</h1>
+            <h1 className="text-5xl text-purple-500 font-bold font-robert-regular">Robert Regular</h1>
+            <h1 className="text-5xl text-red-500 font-bold font-circular-web">Circular Web</h1>
+        </main>
+    )
+}
+
+export default App
